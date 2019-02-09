c56d446fd23eb8da3ad7f17bf09813c9411f5fa1
Thomas Michelet <thomas.michelet@wifirst.fr>
2019-02-09 17:36:54 +0100

    Center app

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {withStyles} from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid'
+import {Grid, Row, Col} from 'react-flexbox-grid';
 
 import NavBar from '../../../components/common/nav-bar'
 import Validation from '../../../components/common/validation'
@@ -8,8 +8,11 @@ import Sidebar from './sidebar'
 import Panels from './panels'
 
 const styles = {
-	root: {
+	grid: {
 		margin: '20px'
+	},
+	sidebar: {
+		paddingLeft: '40px'
 	},
 	validation: {
 		marginTop: '40px'
@@ -43,36 +46,40 @@ class Home extends Component {
 		return (
 			<div>
 				<NavBar/>
-				<div className={classes.root}>
-					<Grid container spacing={24}>
-						<Grid item xs={12} md={6}>
-							<Panels
-								area={area}
-								onChangeArea={(area) => this.setState({area})}
-								equipments={equipments}
-								onChangeEquipments={(equipments) => this.setState({equipments})}
-								windows={windows}
-								onChangeWindows={(windows) => this.setState({windows})}
-								door={door}
-								onChangeDoor={(door) => this.setState({door})}
-								roofing={roofing}
-								onChangeRoofing={(roofing) => this.setState({roofing})}/>
-							<div className={classes.validation}>
-								<div style={{width: '100%'}}>
-									<Validation />
-								</div>
-							</div>
-						</Grid>
-						<Grid item md={6}	>
-							<Sidebar
-								area={area}
-								equipments={equipments}
-								door={door}
-								windows={windows}
-								roofing={roofing} />
-						</Grid>
-					</Grid>
-				</div>
+				<Grid className={classes.grid}>
+					<Row around={"md"}>
+						<Col xs={12} md={8}>
+							<Row>
+								<Col xs={12} md={8}>
+									<Panels
+										area={area}
+										onChangeArea={(area) => this.setState({area})}
+										equipments={equipments}
+										onChangeEquipments={(equipments) => this.setState({equipments})}
+										windows={windows}
+										onChangeWindows={(windows) => this.setState({windows})}
+										door={door}
+										onChangeDoor={(door) => this.setState({door})}
+										roofing={roofing}
+										onChangeRoofing={(roofing) => this.setState({roofing})}/>
+									<div className={classes.validation}>
+										<div style={{width: '100%'}}>
+											<Validation/>
+										</div>
+									</div>
+								</Col>
+								<Col md={4} className={classes.sidebar}>
+									<Sidebar
+										area={area}
+										equipments={equipments}
+										door={door}
+										windows={windows}
+										roofing={roofing}/>
+								</Col>
+							</Row>
+						</Col>
+					</Row>
+				</Grid>
 			</div>
 		)
 	}
