d630e3995dcfb0ab07bf5e9103633269534957b5
Thomas Michelet <thomas.michelet@wifirst.fr>
2019-05-03 08:04:34 +0200

    Use validate.js for forms

--- src/pages/account/login.js
+++ src/pages/account/login.js
@@ -4,12 +4,17 @@ import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 import {withStyles} from '@material-ui/core/styles'
 import Button from "@material-ui/core/Button/Button"
+import validate from "validate.js"
 
 const styles = theme => ({
   card: {
     position: 'absolute',
-    top: '40%',
-    left: '50%'
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: '250px',
+    margin: 'auto'
   },
   form: {
     display: 'flex',
@@ -27,22 +32,58 @@ const styles = theme => ({
   button: {
     marginTop: '20px',
     color: theme.palette.common.white
+  },
+  errors: {
+    marginTop: '15px',
   }
 })
 
-class AccountLogin extends Component {
-  constructor(props) {
-    super(props)
+const validators = {
+  email: {
+    email: {
+      message: 'L\' email n\' est pas valide'
+    }
+  },
+  password: {
+    length: {
+      minimum: 8,
+      tooShort: 'Le mot de passe doit contenir au minimum 8 caractères'
+    }
+  }
+}
 
-    this.state = {
-      email: {
-        value: '',
-        error: false
-      },
-      password: {
-        value: '',
-        error: false
-      }
+const validateField = (field, value) => {
+  return validate({[field]: value}, validators, {fullMessages: false})
+}
+
+class AccountLogin extends Component {
+  static propTypes = {
+    user: PropTypes.object.isRequired,
+    signin: PropTypes.func.isRequired
+  }
+
+  state = {
+    email: {
+      value: '',
+      error: false
+    },
+    password: {
+      value: '',
+      error: false
+    }
+  }
+
+  handleChange = (key, value) => {
+    const result = validateField(key, value)
+    const error = result && result[key].length ? result[key][0] : false
+    this.setState({[key]: {value, error}})
+  }
+
+  handleValid = () => {
+    const {signin} = this.props
+    const {email, password} = this.state
+    if (!(validateField('email', email.value) && validateField('password', password.value))) {
+      signin(email.value, password.value)
     }
   }
 
@@ -81,10 +122,15 @@ class AccountLogin extends Component {
               InputLabelProps={{classes: {root: classes.label}}}
             />
           </div>
-          {user.error ? <Typography>{user.error}</Typography> : ''}
+          {this.renderErrors()}
           <div>
             {!user.isFetching ?
-              <Button variant="contained" color="primary" className={classes.button} onClick={() => this.handleValid()}>
+              <Button
+                disabled={!!(email.error || password.error)}
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                onClick={() => this.handleValid()}>
                 Connexion
               </Button>
             : <Button variant="contained" color="primary" disabled className={classes.button}>
@@ -96,53 +142,25 @@ class AccountLogin extends Component {
     )
   }
 
-  handleChange(key, value) {
-    this.setState({[key]: {value, error: false}})
-  }
-
-  validateField(key) {
-    const field = this.state[key]
-    if (key === 'password') {
-      if (!field.value.length) {
-        this.setState({[key]: {value: field.value, error: true}})
-        return false
-      } else {
-        this.setState({[key]: {value: field.value, error: false}})
-        return true
-      }
-    } else if (key === 'email') {
-      if (!this.validEmail(key)) {
-        this.setState({email: {value: field.value, error: true}})
-        return false
-      } else {
-        this.setState({email: {value: field.value, error: false}})
-        return true
-      }
-    }
-  }
-
-  handleValid() {
-    const {signin} = this.props
+  renderErrors () {
+    const {classes, user} = this.props
     const {email, password} = this.state
-    let isNotValid = !this.validateField('email')
-      || !this.validateField('password')
-
-    if (!isNotValid) {
-      signin(email.value, password.value)
+    if (user.error || email.error || password.error) {
+      return (
+        <div className={classes.errors}>
+          {email.error ?
+            <Typography>{email.error}</Typography>
+            : null}
+          {password.error ?
+            <Typography>{password.error}</Typography>
+            : null}
+          {user.error ?
+            <Typography>{user.error}</Typography>
+            : null}
+        </div>
+      )
     }
   }
-
-  validEmail(key) {
-    const email = this.state[key]
-    // eslint-disable-next-line
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return re.test(email.value.toLowerCase());
-  }
-}
-
-AccountLogin.propTypes = {
-  user: PropTypes.object.isRequired,
-  signin: PropTypes.func.isRequired
 }
 
 export default withStyles(styles)(AccountLogin)
