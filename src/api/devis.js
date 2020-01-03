58737e66864790315b0bb635db8ddfc51965c388
Thomas Michelet <thomas.michelet@wifirst.fr>
2020-01-03 14:21:40 +0100

    Add some log

--- src/api/devis.js
+++ src/api/devis.js
@@ -20,7 +20,7 @@ export const list = () => {
 
 export const calculateArea = (area) => {
 	const {largeur, longueur} = area
-	console.log('calculateArea', area)
+	console.log('calculateAreas', area)
 	return Math.ceil((largeur * longueur) * PRICES['area'])
 }
 
